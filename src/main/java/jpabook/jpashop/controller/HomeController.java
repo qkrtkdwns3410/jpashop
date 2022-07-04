26284ddfe454d940391de6d7efdc697db68732e3
jihye94 <qkrtkdwns3410@naver.com>
2022-07-04 13:09:08 +0900

    06/20/22

---
+++ src/main/java/jpabook/jpashop/controller/HomeController.java
@@ -0,0 +1,16 @@
+package jpabook.jpashop.controller;
+
+/**
+ * packageName    : jpabook.jpashop.controller
+ * fileName       : HomeController
+ * author         : jihye94
+ * date           : 2022-07-04
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022-07-04        jihye94       최초 생성
+ */
+public class HomeController {
+
+}
