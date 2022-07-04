10fe73204e3eb56db4c98f3e75d0486b77f8a324
jihye94 <qkrtkdwns3410@naver.com>
2022-07-04 14:11:09 +0900

    06/20/22

---
+++ src/main/java/jpabook/jpashop/confg/LayOutConfig.java
@@ -0,0 +1,28 @@
+package jpabook.jpashop.confg;
+
+import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+/**
+ * packageName    : jpabook.jpashop.confg
+ * fileName       : LayOutConfig
+ * author         : jihye94
+ * date           : 2022-07-04
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022-07-04        jihye94       최초 생성
+ */
+@Configuration
+public class LayOutConfig {
+      
+      //thymeleaf layout
+      @Bean
+      public LayoutDialect layoutDialect() {
+            return new LayoutDialect();
+      }
+      
+}
--- src/main/java/jpabook/jpashop/controller/HomeController.java
+++ src/main/java/jpabook/jpashop/controller/HomeController.java
@@ -1,5 +1,12 @@
 package jpabook.jpashop.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
 /**
  * packageName    : jpabook.jpashop.controller
  * fileName       : HomeController
@@ -11,6 +18,15 @@ package jpabook.jpashop.controller;
  * -----------------------------------------------------------
  * 2022-07-04        jihye94       최초 생성
  */
+@Controller
+@Slf4j
 public class HomeController {
-
+      
+      @RequestMapping("/")
+      public String home() {
+            log.info("HomeController.home ");
+            
+            return "home";
+      }
+      
 }
